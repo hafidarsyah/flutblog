873fe8665860f8b375e2c32e493080da5f6a7c80
Hafid Ardiansyah <62227964+hafidardiansyah@users.noreply.github.com>
2021-03-28 22:30:39 +0700

    Add service and models

---
+++ app/lib/service/post_service.dart
@@ -0,0 +1,15 @@
+import 'package:http/http.dart' as http;
+import '../models/post_model.dart';
+
+class PostService {
+  final Uri baseURL = Uri.parse('http://127.0.0.1:8000/api/posts');
+
+  Future<List<PostModel>> getPosts() async {
+    final response = await http.get(baseURL);
+
+    if (response.statusCode == 200) {
+      return postFromJson(response.body);
+    }
+    return null;
+  }
+}
